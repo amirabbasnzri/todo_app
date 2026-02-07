06383c2e22f10fad4f90076e83a8718202ee19f1
amirabbs <amirabbasnzri8@gmail.com>
2026-02-07 16:18:45 +0330

    add user-register route

--- app/routes/users.py
+++ app/routes/users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Path, Depends, HTTPException, Query, status
+from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 from app.models.users import UserModel
@@ -7,7 +7,7 @@ from app.schemas.users import *
 from passlib.context import CryptContext
 
 
-user_router = APIRouter()
+user_router = APIRouter(tags=['users'])
 
 
 # hash password:
@@ -25,16 +25,25 @@ def user_register(request: UserCreateSchema, db: Session = Depends(get_db)):
     if db_user_email:
         raise HTTPException(detail='Email already registered', status_code=status.HTTP_400_BAD_REQUEST)
     
+    # password validation:
+    if request.password != request.confirm_password:
+        raise HTTPException(detail='password and confirm_password do not match', status_code=status.HTTP_400_BAD_REQUEST)
+    
     # hashing password:
     hashed_password = get_password_hash(request.password)
-    
+   
     # create user:
     user = UserModel(name=request.name, email=request.email, hashed_password=hashed_password)
     db.add(user)
     db.commit()
     db.refresh(user)
-    
-    return JSONResponse(content={'Server response': f'User {user.name} with email {user.email} and id {user.id} created successfully'})        
+    # successful response:
+    return JSONResponse(
+        content={'Server response': 
+        f'User <{user.name}> with email <{user.email}> and id <{user.id}> created successfully'
+        },
+        status_code=status.http_201_created
+        )        
     
     
     
--- app/schemas/users.py
+++ app/schemas/users.py
@@ -5,6 +5,7 @@ class UserBaseSchema(BaseModel):
     name: str = Field(..., min_length=8, max_length=256)
     email: str = EmailStr()
     password: str = Field(..., min_length=8, max_length=128)
+    confirm_password: str = Field(..., min_length=8, max_length=128)
     
 class UserCreateSchema(UserBaseSchema):
     pass
