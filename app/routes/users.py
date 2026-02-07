4498e42fda5ab4414cf84f4b62c4e86bfca9c548
amirabbs <amirabbasnzri8@gmail.com>
2026-02-07 18:04:47 +0330

    add user login route-schema

--- app/routes/users.py
+++ app/routes/users.py
@@ -18,7 +18,7 @@ def get_password_hash(password: str):
 
 # registration:
 @user_router.post('/register')
-def user_register(request: UserCreateSchema, db: Session = Depends(get_db)):
+def user_register(request: UserRegisterSchema, db: Session = Depends(get_db)):
    
     # email validation:
     db_user_email = db.query(UserModel).filter(UserModel.email == request.email).first()
@@ -28,6 +28,12 @@ def user_register(request: UserCreateSchema, db: Session = Depends(get_db)):
     # password validation:
     if request.password != request.confirm_password:
         raise HTTPException(detail='password and confirm_password do not match', status_code=status.HTTP_400_BAD_REQUEST)
+    elif not request.strong_password():
+        raise HTTPException(
+            detail='Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character (like: !@#$%^&*)',
+            status_code=status.HTTP_400_BAD_REQUEST
+            )
+    
     
     # hashing password:
     hashed_password = get_password_hash(request.password)
@@ -39,14 +45,31 @@ def user_register(request: UserCreateSchema, db: Session = Depends(get_db)):
     db.refresh(user)
     # successful response:
     return JSONResponse(
-        content={'Server response': 
-        f'User <{user.name}> with email <{user.email}> and id <{user.id}> created successfully'
+        content={'Server response': f'User <{user.name}> with email <{user.email}> and id <{user.id}> created successfully'
         },
-        status_code=status.http_201_created
+        status_code=status.HTTP_201_CREATED
         )        
     
     
+
+@user_router.post('/login')
+def user_login(request: UserLoginSchema, db: Session = Depends(get_db)):
     
+    # email validation:
+    db_user = db.query(UserModel).filter(UserModel.email == request.email).first()
+    if not db_user:
+        raise HTTPException(detail='Email not found', status_code=status.HTTP_404_NOT_FOUND)
+    
+    # password validation:
+    if not pwd_context.verify(request.password, db_user.hashed_password):
+        raise HTTPException(detail='Incorrect password', status_code=status.HTTP_400_BAD_REQUEST)
+    
+    # successful response:
+    return JSONResponse(
+        content={'Server response': f'User with email <{db_user.email}> logged in successfully'
+        },
+        status_code=status.HTTP_200_OK
+        )
     
     
 
--- app/schemas/users.py
+++ app/schemas/users.py
@@ -2,10 +2,21 @@ from pydantic import BaseModel, EmailStr, Field
 
 
 class UserBaseSchema(BaseModel):
-    name: str = Field(..., min_length=8, max_length=256)
     email: str = EmailStr()
     password: str = Field(..., min_length=8, max_length=128)
-    confirm_password: str = Field(..., min_length=8, max_length=128)
     
-class UserCreateSchema(UserBaseSchema):
+class UserRegisterSchema(UserBaseSchema):
+
+    name: str = Field(..., min_length=8, max_length=256)
+    confirm_password: str = Field(..., min_length=8, max_length=128)
+
+    def strong_password(self) -> bool:
+        has_upper = any(char.isupper() for char in self.password)
+        has_lower = any(char.islower() for char in self.password)
+        has_digit = any(char.isdigit() for char in self.password)
+        has_special = any(not char.isalnum() for char in self.password)
+        return all([has_upper, has_lower, has_digit, has_special])
+    
+    
+class UserLoginSchema(UserBaseSchema):
     pass
